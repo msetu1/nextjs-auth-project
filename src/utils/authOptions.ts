4480f252cf4791d8b976f54e60bb8bc458aaf46e
msetu1 <msetu5763@gmail.com>
2025-02-24 23:50:49 +0600

    github login related code

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,12 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  // your configs
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
